2602a24abae4f3778d3ed9ca196ccbe26666b860
Milan De Buck <milan.debuck@student.odisee.be>
2017-01-08 00:07:17 +0100

    oops left a small mistake from a previous attempt open

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
-var helpers = require('./helpers');
 const path = require('path');
 const rootDir = path.resolve(__dirname, '..');
 
